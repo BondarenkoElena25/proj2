c0e01bed1581f2599c7da9c8bcccb926622ec4bb
Ja. Harochau <harochau@gmail.com>
2021-08-04 20:39:12 +0300

    created information

--- block/urls.py
+++ block/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from block.views import home, about
+from block.views import home, about, info
 
 urlpatterns = [
     path('', home),
-    path('about', about)
+    path('about', about),
+    path('info', info)
 ]
